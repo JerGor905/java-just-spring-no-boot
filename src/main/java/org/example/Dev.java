d020788cd35fb67b38ff5fe80545c0c4f0ce7a16
Jeffery Tse <kytseag@ust.hk>
2024-07-22 01:18:19 +0800

    feat: add Laptop class

--- src/main/java/org/example/Dev.java
+++ src/main/java/org/example/Dev.java
@@ -1,9 +1,11 @@
 package org.example;
 
 public class Dev {
+    public Dev() {
+        System.out.println("Dev Constructor");
+    }
 
     public void build() {
         System.out.println("Working on an AWESOME project!");
     }
-
 }
