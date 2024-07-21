559e5ed669b25538879c33bc2a1663ccc0ed1a0d
Jeffery Tse <kytseag@ust.hk>
2024-07-22 01:06:56 +0800

    feat: add Spring xml config

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,14 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Dev obj = (Dev) context.getBean("dev");
+        obj.build();
     }
 }
