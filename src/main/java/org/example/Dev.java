d50661977ed21ab540045c91b8d3b9ee54956a5d
Jeffery Tse <kytseag@ust.hk>
2024-07-22 01:47:38 +0800

    feat: implement Setter Injection for Dev

--- src/main/java/org/example/Dev.java
+++ src/main/java/org/example/Dev.java
@@ -1,11 +1,23 @@
 package org.example;
 
 public class Dev {
+//    private Laptop laptop;
+    private int age;
+
     public Dev() {
         System.out.println("Dev Constructor");
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public void build() {
         System.out.println("Working on an AWESOME project!");
+//        laptop.compile();
     }
 }
